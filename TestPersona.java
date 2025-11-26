da826b7e136de483c1b44a84c1656be399094a9c
Beatris Macovei <beatriz1308m.m@gmail.com>
2025-11-26 12:14:06 +0100

    Update TestPersona.java

--- TestPersona.java
+++ TestPersona.java
@@ -6,13 +6,14 @@ public class TestPersona
     CuentaBancaria cuenta1 = new CuentaBancaria();
     CuentaBancaria cuenta2 = new CuentaBancaria();
     
+    
     String nombre = usuario.getNombre();
     System.out.println("Nombre usuario:"+" "+usuario.getNombre());
     
     String apellidos = usuario.getApellidos();
     System.out.println("Apellido usuario:"+" "+usuario.getApellidos());
     
-    usuario.setTelefono("638714927");
+    usuario.setTelefono(654567865);
     System.out.println("Telefono usuario:"+" "+usuario.getTelefono());
     
     String direccion = usuario.getDireccion();
@@ -22,13 +23,13 @@ public class TestPersona
     System.out.println("Año de nacimiento del usuario:"+" "+usuario.getAñoNacimiento());
 
     cuenta1.tarjetaTitular();
-    System.out.println("Este es el titular de la cuenta"+titular);
-    cuenta1.ingresoCuenta();
-     System.out.println("Este es el dinero que desea ingresar:"+ingreso);
-    cuenta1.retiroCuenta();
-     System.out.println("Este es el dinero que desea retirar:"+sacar);
+    System.out.println("Este es el titular de la cuenta"+cuenta1.tarjetaTitular());
+    double ing = cuenta1.ingresoCuenta();
+     System.out.println("Este es el dinero que desea ingresar:"+ing);
+    boolean ret = cuenta1.retiroCuenta(5);
+     System.out.println("¿Ha podido sacar dinero?"+" "+ret);
     cuenta1.getSaldo();
-     System.out.println("Este es el saldo de la cuenta 1:"+getSaldo());
+     System.out.println("Este es el saldo de la cuenta 1:"+cuenta1.getSaldo());
     
   }
 }
